8322f350e8da3a90c43adc9fc35ff0d12db445ed
manas-vyas <66221115+manas-vyas@users.noreply.github.com>
2020-07-12 19:46:59 +0530

    Add files via upload

---
+++ MavenTestng/MavenTestng/src/test/java/MavenTestng/MavenTestng/checkone.java
@@ -0,0 +1,11 @@
+package MavenTestng.MavenTestng;
+
+import org.testng.annotations.Test;
+
+public class checkone {
+	@Test
+	public void test5() {
+		System.out.println("test5");
+	}
+
+}
---
+++ MavenTestng/MavenTestng/src/test/java/MavenTestng/MavenTestng/testngchecking.java
@@ -0,0 +1,20 @@
+package MavenTestng.MavenTestng;
+
+import org.testng.annotations.Test;
+
+public class testngchecking {
+		
+	@Test
+	public void test1() {
+		System.out.println("test1");
+	}
+	@Test
+	public void test2() {
+		System.out.println("test2");
+		
+	}
+	@Test
+	public void test3() {
+		System.out.println("test3");
+	}
+}
